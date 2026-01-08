e3266dd7b625f233cacc0869ed99e84105f8059b
Kirankumar866 <Kirankumarparasa9996@gmail.com>
2026-01-08 14:28:56 -0500

    added update and delete

--- src/main/java/com/taskify/taskify_api/controller/TaskController.java
+++ src/main/java/com/taskify/taskify_api/controller/TaskController.java
@@ -17,17 +17,42 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    // Add a task
+    /** View all tasks
+     * LOCAL HOST URL FOR POSTMAN    POST METHOD  http://localhost:8080/tasks
+     **/
     @PostMapping
     public ResponseEntity<Task> addTask(@RequestBody Task task) {
         Task savedTask = taskService.createTask(task);
         return ResponseEntity.ok(savedTask);
     }
 
-    // View all tasks
+    /** View all tasks
+     * LOCAL HOST URL FOR POSTMAN  GET METHOD http://localhost:8080/tasks
+    **/
     @GetMapping
     public ResponseEntity<List<Task>> viewTasks() {
         List<Task> tasks = taskService.getAllTasks();
         return ResponseEntity.ok(tasks);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.ok("Deleted");
+    }
+
+    @PatchMapping("/updatetask/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,
+                                           @RequestBody Task updatedtask){
+        return ResponseEntity.ok(taskService.patchTask(id, updatedtask));
+    }
+
+
+
+
 }
