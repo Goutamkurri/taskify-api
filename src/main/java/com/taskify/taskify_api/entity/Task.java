34660ae0816d698f681aa4e847c7de7a261332b2
Goutam Kurri <kgoutam44@gmail.com>
2026-01-07 18:04:57 -0600

    Initial stable Spring Boot setup with H2 DB, ready for Task API

---
+++ src/main/java/com/taskify/taskify_api/entity/Task.java
@@ -0,0 +1,27 @@
+package com.taskify.taskify_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
